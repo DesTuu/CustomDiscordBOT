c4ee7d2dd411eaf5ae2ab3a871bd9d3910e0f2f5
DesTuu <maurycy-vip@wp.pl>
2024-03-16 21:24:56 +0100

    Base004

--- app/settings.py
+++ app/settings.py
@@ -1,7 +1,7 @@
 import pathlib
 
-BASE_DIR = pathlib.Path(__file__).parent
-
-CMD_DIR = BASE_DIR / "cmds"
+# BASE_DIR = pathlib.Path(__file__).parent
+#
+# CMD_DIR = BASE_DIR / "cogs"
 
 COMMAND_PREFIX = f"!"
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import app
-import os
+from my_token import DISCORD_BOT_TOKEN
 
 intents = discord.Intents.all()
 intents.messages = True
@@ -12,11 +12,10 @@ bot = commands.Bot(command_prefix=app.COMMAND_PREFIX, intents=intents, activity=
 async def on_ready():
     # for cmd_file in app.CMD_DIR.glob('*.py'):
     #     if cmd_file.name != '__init__.py':
-    #         await bot.load_extension(f"cmds.{cmd_file.name[:-3]}")
+    #         await bot.load_extension(f"cogs.{cmd_file.name[:-3]}")
 
     await bot.load_extension("cogs.example")
 
 
 if __name__ == "__main__":
-    bot.run(os.environ['DISCORD_TOKEN'])
-
+    bot.run(DISCORD_BOT_TOKEN)
