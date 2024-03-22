9aa22dd457e96471cc8f0b5fc1a0bd7708443918
DesTuu <maurycy-vip@wp.pl>
2024-03-22 09:57:15 +0100

    NewChannelMessage

--- cogs/new_channel_message.py
+++ cogs/new_channel_message.py
@@ -1,14 +1,16 @@
 from discord.ext import commands
 
-class Queue_message(commands.Cog):
+class New_channel_message(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.Cog.listener()
     async def on_guild_channel_create(self, channel):
-        # guild_id = "979427975692947546"
         if "queue" in channel.name:
             await channel.send("Przykładowa wiadomość")
 
+        if channel.category.name == "Weryfikacja":
+            await channel.send("Nowy kanał w kategorii Weryfikacja")
+
 async def setup(bot):
-    await bot.add_cog(Queue_message(bot))
+    await bot.add_cog(New_channel_message(bot))
